7ae920b858a7a0e07d1f95d287a38173e033f1df
dbryandallas <dustin.bryan@gmail.com>
2016-04-12 19:42:46 -0400

    HadoukenAnimation

--- js/app.js
+++ js/app.js
@@ -1,5 +1,29 @@
 $(document).ready(function() {
   $('.ryu').mouseenter(function() {
-    alert('mouse entered .ryu div');
+  	$('.ryu-still').hide();
+    $('.ryu-ready').show();    
+  })
+  .mouseleave(function() {
+    $('.ryu-ready').hide();
+    $('.ryu-still').show();
+  })
+  .mousedown(function() {
+  // play hadouken sound
+  $('.ryu-ready').hide();
+  $('.ryu-throwing').show();
+  $('.hadouken').show();
+  // animate hadouken to the right of the screen
+  })
+  .mouseup(function() {
+  $('.ryu-throwing').hide();
+  $('.ryu-ready').show();
   });
 });
+  $('.hadouken').show().animate(
+  {'left': '1020px'},
+  500,
+  function() {
+  $(this).hide();
+  $(this).css('left', '520px');
+  }
+);
